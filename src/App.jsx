38bdcdaa0c1e1ba93bf644a6a772edc0521faaff
AdzhanSerik <adzhan.serik@gmail.com>
2024-08-20 21:38:27 +0500

    asdasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
-import data from './data.json'
+// import data from './data.json'
 import Header from './components/Header'
 import Category from './components/Category'
 import Sort from './components/Sort'
 import PizzaList from './components/PizzaList'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Sceleton from './components/Sceleton'
+
 
 function App() {
 
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+  const scelet = Array(8).fill(0)
+
+  useEffect(() => {
+    axios
+      .get("https://c12550f372786959.mokky.dev/items")
+      .then((res) => {
+        setData(res.data)
+        setIsLoading(false)
+      })
+
+  }, []);
 
   return (
     <div className='bg-orange-300 p-10'>
@@ -19,10 +36,13 @@ function App() {
         <div className='mt-10'>
           <div className='mb-10'><h2 className='font-bold text-3xl'>Все пиццы</h2></div>
           <div className='grid grid-cols-4 gap-5'>
+
             {
-              data.map((pizza) => (
-                <PizzaList key={pizza.id} {...pizza} />
-              ))
+              isLoading ?
+                scelet.map((value, index) => <Sceleton key={index} />) :
+                data.map((pizza) => (
+                  <PizzaList key={pizza.id} {...pizza} />
+                ))
             }
 
           </div>
--- src/components/PizzaList.jsx
+++ src/components/PizzaList.jsx
@@ -4,10 +4,11 @@ import plusOrange from '/plusOrange.svg'
 
 
 
-export default function PizzaList({ imageUrl, name }) {
 
-    const [doughType, setDoughType] = useState('тонкое');
-    const [size, setSize] = useState('26 см.');
+export default function PizzaList({ imageUrl, name, price, types, sizes }) {
+    const [sizePizza, setSizePizza] = useState(0);
+    const [typePizza, setTypePizza] = useState(0);
+    const typePizzas = ['Традиционное', 'Тонкое'];
     return (
 
         <>
@@ -17,40 +18,39 @@ export default function PizzaList({ imageUrl, name }) {
                 <h3 className='font-bold text-xl'>{name}</h3>
                 <div className="p-4 bg-gray-100 rounded-xl">
                     <div className="grid grid-cols-2 gap-4 mb-4">
-                        <button
-                            className={`py-2 px-4 rounded-lg font-medium ${doughType === 'тонкое' ? 'bg-white shadow' : 'bg-gray-200'}`}
-                            onClick={() => setDoughType('тонкое')}>
-                            тонкое
-                        </button>
-                        <button
-                            className={`py-2 px-2 rounded-lg font-medium ${doughType === 'традиционное' ? 'bg-white shadow' : 'bg-gray-200'}`}
-                            onClick={() => setDoughType('традиционное')}>
-                            традиционное
-                        </button>
+                        {
+                            types.map((type, index) => (
+                                <button
+                                    key={index}
+                                    className={typePizza === index ? `py-2 px-4 rounded-lg font-medium bg-gray-200` : `py-2 px-4 rounded-lg font-medium bg-white`}
+
+                                    onClick={() => setTypePizza(index)}>
+                                    {typePizzas[type]}
+                                </button>
+                            ))
+                        }
+
                     </div>
                     <div className="grid grid-cols-3 gap-4">
-                        <button
-                            className={`py-2 px-3 rounded-lg font-medium ${size === '26 см.' ? 'bg-white shadow' : 'bg-gray-200'}`}
-                            onClick={() => setSize('26 см.')}>
-                            26 см.
-                        </button>
-                        <button
-                            className={`py-2 px-3 rounded-lg font-medium ${size === '30 см.' ? 'bg-white shadow' : 'bg-gray-200'}`}
-                            onClick={() => setSize('30 см.')}>
-                            30 см.
-                        </button>
-                        <button
-                            className={`py-2 px-3 rounded-lg font-medium ${size === '40 см.' ? 'bg-white shadow' : 'bg-gray-200'}`}
-                            onClick={() => setSize('40 см.')}>
-                            40 см.
-                        </button>
+                        {
+                            sizes.map((size, index) => (
+                                <button
+                                    key={index}
+                                    className={sizePizza === index ? `py-2 px-3 rounded-lg font-medium bg-gray-200` : `py-2 px-3 rounded-lg font-medium bg-white`}
+                                    onClick={() => setSizePizza(index)}
+                                >
+                                    {size} см.
+                                </button>
+                            ))
+                        }
+
                     </div>
                 </div>
 
 
-                <div className='flex justify-between items-center w-full'>
-                    <span className='font-bold text-xl'>от 395 тг</span>
-                    <div className='border border-orange-500 rounded-full flex items-center py-2 px-4 gap-2 text-orange-500 font-bold'>
+                <div className='flex justify-evenly items-center w-full'>
+                    <span className='font-bold text-xl'>от {price} тг</span>
+                    <div className='cursor-pointer border border-orange-500 rounded-full flex items-center py-2 px-4 gap-2 text-orange-500 font-bold'>
                         <img src={plusOrange} alt="" />
                         <span>Добавить  </span>
                     </div>
